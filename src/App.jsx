e99164424342b79c88d554372295f510b819352f
Marin-Andrei <andreimarinjunior@gmail.com>
2024-09-18 20:37:33 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/Main";
+
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
+};
+
+export default App;
